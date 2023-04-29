573ba544c50d15690400bcbee55459f7638a795e
Matt Hoffner <matthoffner@gmail.com>
2023-04-29 10:41:31 -0700

    Update server.py

--- server.py
+++ server.py
@@ -27,63 +27,11 @@ class LlamaArgs:
     a = "Alice"
     b = "Bob"
     i = 10000
-    activity = "coming up with prompt ideas for this server"
     model_path = ""
     a_starter = ""
     b_starter = ""
 
 args = LlamaArgs()
-
-def get_message(service_context):
-    a_user_prefix_tmpl = (
-        f"Your name is {args.a}. "
-        f"You are {args.activity} with another person named {args.b}. "
-        f"We provide conversation context between you and {args.b} below. "
-    )
-    b_user_prefix_tmpl = (
-        f"Your name is {args.b}. "
-        f"You are {args.activity} with another person named {args.a}. "
-        f"We provide conversation context between you and {args.a} below. "
-    )
-    a_agent = ConvoAgent.from_defaults(
-        name=args.a, 
-        service_context=service_context, 
-        user_prefix_tmpl=a_user_prefix_tmpl,
-        lt_memory=GPTListIndex.from_documents([], service_context=service_context)
-    )
-    b_agent = ConvoAgent.from_defaults(
-        name=args.b, 
-        service_context=service_context,
-        user_prefix_tmpl=b_user_prefix_tmpl,
-        lt_memory=GPTListIndex.from_documents([], service_context=service_context)
-    )
-    a_starter = args.a_starter or f"Hi, my name is {args.a}!"
-    b_starter = args.b_starter or f"Hi, my name is {args.b}!" 
-
-    a_agent.add_message(a_starter, args.a)
-    b_agent.add_message(a_starter, args.a)
-
-    a_agent.add_message(b_starter, args.b)
-    b_agent.add_message(b_starter, args.b)
-    
-    # run conversation loop
-    current_user = args.a
-    message = []
-    for _ in range(args.i):
-
-        agent = a_agent if current_user == args.a else b_agent
-        new_message = agent.generate_message(service_context)
-
-        message_to_print = format_text(new_message, current_user)
-        print(message_to_print)
-
-        a_agent.add_message(new_message, current_user)
-        b_agent.add_message(new_message, current_user)
-
-        current_user = args.a if current_user == args.b else args.b
-        message.append(message_to_print)
-    
-    return message
 
 
 @app.get("/about")
@@ -103,47 +51,47 @@ async def index(request: Request):
 
 @app.get("/")
 @app.get("/stream")
-async def message_stream(request: Request):
+async def message_stream(activity = "creating a video game", a = "Alice", b = "Bob"):
     service_context = get_service_context(model_path=args.model_path)
     async def event_generator(service_context):
         a_user_prefix_tmpl = (
-                f"Your name is {args.a}. "
-                f"You are {args.activity} with another person named {args.b}. "
-                f"We provide conversation context between you and {args.b} below. "
+                f"Your name is {a}. "
+                f"You are {activity} with another person named {b}. "
+                f"We provide conversation context between you and {b} below. "
             )
         b_user_prefix_tmpl = (
-                f"Your name is {args.b}. "
-                f"You are {args.activity} with another person named {args.a}. "
-                f"We provide conversation context between you and {args.a} below. "
+                f"Your name is {b}. "
+                f"You are {activity} with another person named {a}. "
+                f"We provide conversation context between you and {a} below. "
             )
 
         a_lt_memory = GPTListIndex.from_documents([], service_context=service_context)
         b_lt_memory = GPTListIndex.from_documents([], service_context=service_context)
         a_agent = ConvoAgent.from_defaults(
-                name=args.a, 
+                name=a, 
                 service_context=service_context, 
                 user_prefix_tmpl=a_user_prefix_tmpl,
                 lt_memory=a_lt_memory
             )
         b_agent = ConvoAgent.from_defaults(
-            name=args.b, 
+            name=b, 
             service_context=service_context,
             user_prefix_tmpl=b_user_prefix_tmpl,
             lt_memory=b_lt_memory
         )
-        a_starter = args.a_starter or f"Hi, my name is {args.a}!"
-        b_starter = args.b_starter or f"Hi, my name is {args.b}!" 
+        a_starter = args.a_starter or f"Hi, my name is {a}!"
+        b_starter = args.b_starter or f"Hi, my name is {b}!" 
 
-        a_agent.add_message(a_starter, args.a)
-        b_agent.add_message(a_starter, args.a)
+        a_agent.add_message(a_starter, a)
+        b_agent.add_message(a_starter, a)
 
-        a_agent.add_message(b_starter, args.b)
-        b_agent.add_message(b_starter, args.b)
+        a_agent.add_message(b_starter, b)
+        b_agent.add_message(b_starter, b)
         
         # run conversation loop
-        current_user = args.a
+        current_user = a
         for _ in range(args.i):
-            agent = a_agent if current_user == args.a else b_agent
+            agent = a_agent if current_user == a else b_agent
             new_message = agent.generate_message(service_context)
 
             message_to_print = format_text(new_message, current_user)
@@ -153,7 +101,7 @@ async def message_stream(request: Request):
             a_agent.add_message(new_message, current_user)
             b_agent.add_message(new_message, current_user)
 
-            current_user = args.a if current_user == args.b else args.b
+            current_user = a if current_user == b else b
 
     return StreamingResponse(event_generator(service_context))
 
