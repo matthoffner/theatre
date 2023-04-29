af340191e47200eb23c6f991e429338859bcae61
Matt Hoffner <matthoffner@gmail.com>
2023-04-29 09:07:37 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,24 +3,16 @@ import logging
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from llama_index import GPTListIndex
-from sse_starlette.sse import EventSourceResponse
+from fastapi.responses import StreamingResponse, HTMLResponse
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 from main import ConvoAgent, format_text, get_service_context
 
-import asyncio
-import uvicorn
-
-
-
 logger = logging.getLogger()
 
-MESSAGE_STREAM_DELAY = .5  # second
-MESSAGE_STREAM_RETRY_TIMEOUT = 15000  # milisecond
 app = FastAPI()
-
-app.mount("/js", StaticFiles(directory="js"), name="js")
 
 # add CORS so our web page can connect to our api
 app.add_middleware(
@@ -31,13 +23,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-COUNTER = 0
-
 class LlamaArgs:
     a = "Alice"
-    b = "Ben"
-    i = 1000
-    activity = "creating a video game"
+    b = "Bob"
+    i = 10000
+    activity = "coming up with prompt ideas for this server"
     model_path = ""
     a_starter = ""
     b_starter = ""
@@ -96,7 +86,7 @@ def get_message(service_context):
     return message
 
 
-@app.get("/")
+@app.get("/about")
 async def index(request: Request):
     # prompt = request.query_params.get('prompt')
     html_content = f"""
@@ -106,78 +96,66 @@ async def index(request: Request):
         </head>
         <body>
             <h1 style="text-align:center;font-family:Arial">theatre</h1>
-            <div id="root"></div>
-            <script type="module" src="/js/index.js"></script>
         </body>
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
 
-
+@app.get("/")
 @app.get("/stream")
 async def message_stream(request: Request):
     service_context = get_service_context(model_path=args.model_path)
     async def event_generator(service_context):
-        while True:
-            if await request.is_disconnected():
-                logger.debug("Request disconnected")
-                break
-
-            a_user_prefix_tmpl = (
+        a_user_prefix_tmpl = (
                 f"Your name is {args.a}. "
                 f"You are {args.activity} with another person named {args.b}. "
                 f"We provide conversation context between you and {args.b} below. "
             )
-            b_user_prefix_tmpl = (
+        b_user_prefix_tmpl = (
                 f"Your name is {args.b}. "
                 f"You are {args.activity} with another person named {args.a}. "
                 f"We provide conversation context between you and {args.a} below. "
             )
-            a_agent = ConvoAgent.from_defaults(
+
+        a_lt_memory = GPTListIndex.from_documents([], service_context=service_context)
+        b_lt_memory = GPTListIndex.from_documents([], service_context=service_context)
+        a_agent = ConvoAgent.from_defaults(
                 name=args.a, 
                 service_context=service_context, 
                 user_prefix_tmpl=a_user_prefix_tmpl,
-                lt_memory=GPTListIndex.from_documents([], service_context=service_context)
+                lt_memory=a_lt_memory
             )
-            b_agent = ConvoAgent.from_defaults(
-                name=args.b, 
-                service_context=service_context,
-                user_prefix_tmpl=b_user_prefix_tmpl,
-                lt_memory=GPTListIndex.from_documents([], service_context=service_context)
-            )
-            a_starter = args.a_starter or f"Hi, my name is {args.a}!"
-            b_starter = args.b_starter or f"Hi, my name is {args.b}!" 
+        b_agent = ConvoAgent.from_defaults(
+            name=args.b, 
+            service_context=service_context,
+            user_prefix_tmpl=b_user_prefix_tmpl,
+            lt_memory=b_lt_memory
+        )
+        a_starter = args.a_starter or f"Hi, my name is {args.a}!"
+        b_starter = args.b_starter or f"Hi, my name is {args.b}!" 
 
-            a_agent.add_message(a_starter, args.a)
-            b_agent.add_message(a_starter, args.a)
+        a_agent.add_message(a_starter, args.a)
+        b_agent.add_message(a_starter, args.a)
 
-            a_agent.add_message(b_starter, args.b)
-            b_agent.add_message(b_starter, args.b)
-            
-            # run conversation loop
-            current_user = args.a
-            for _ in range(args.i):
-                agent = a_agent if current_user == args.a else b_agent
-                new_message = agent.generate_message(service_context)
+        a_agent.add_message(b_starter, args.b)
+        b_agent.add_message(b_starter, args.b)
+        
+        # run conversation loop
+        current_user = args.a
+        for _ in range(args.i):
+            agent = a_agent if current_user == args.a else b_agent
+            new_message = agent.generate_message(service_context)
 
-                message_to_print = format_text(new_message, current_user)
-                print(message_to_print)
+            message_to_print = format_text(new_message, current_user)
+            yield(message_to_print)
+            print(message_to_print)
 
-                a_agent.add_message(new_message, current_user)
-                b_agent.add_message(new_message, current_user)
+            a_agent.add_message(new_message, current_user)
+            b_agent.add_message(new_message, current_user)
 
-                current_user = args.a if current_user == args.b else args.b
+            current_user = args.a if current_user == args.b else args.b
 
-                yield {
-                    "event": "new_message",
-                    "id": "message_id",
-                    "retry": MESSAGE_STREAM_RETRY_TIMEOUT,
-                    "data": message_to_print,
-                }
-
-                await asyncio.sleep(MESSAGE_STREAM_DELAY)
-
-    return EventSourceResponse(event_generator(service_context))
+    return StreamingResponse(event_generator(service_context))
 
 
 if __name__ == "__main__":
